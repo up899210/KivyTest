9ea2c787ceddf69e6e3e0569340bff9947223797
up899210 <60466481+up899210@users.noreply.github.com>
2020-09-05 13:48:38 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 2 # Amounts of columns = 2
+        self.add_widget(Label(text="First Name: "))
+        self.FirstName = TextInput(multiline=False)
+        self.add_widget(self.FirstName)
+
+        self.add_widget(Label(text="Last Name: "))
+        self.LastName = TextInput(multiline=False)
+        self.add_widget(self.LastName)
+
+        self.add_widget(Label(text="Email: "))
+        self.Email = TextInput(multiline=False)
+        self.add_widget(self.Email)
+
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+if __name__ == '__main__':
+   MyApp().run()
